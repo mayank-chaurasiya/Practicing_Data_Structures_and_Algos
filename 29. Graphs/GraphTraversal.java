651a7511114200ffe4206b51fe134899224f9f4e
Mayank <mayankchaurasiya66@gmail.com>
2025-07-04 14:27:08 +0530

    Added Graph Traversal logic

---
+++ 29. Graphs/GraphTraversal.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class GraphTraversal {
+    static class Edge {
+        int src, dest, wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < verts; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void main(String[] args) {
+        int verts = 7;
+        ArrayList<Edge>[] graph = new ArrayList[verts];
+
+        createGraph(graph);
+    }
+}
