8b728e69211eee6bf63b74db97d39daa63bff4cb
Mayank <mayankchaurasiya66@gmail.com>
2025-07-11 14:18:53 +0530

    Added isBipartite function in the BipartiteGraph file

--- 29. Graphs/BipartiteGraph.java
+++ 29. Graphs/BipartiteGraph.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class BipartiteGraph {
     static class Edge {
@@ -31,9 +32,41 @@ public class BipartiteGraph {
         graph[4].add(new Edge(4, 3));
     }
 
+    public static boolean isBipartite(ArrayList<Edge>[] graph) {
+        int color[] = new int[graph.length];
+
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1; // no color
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == -1) {
+                q.add(i);
+                color[i] = 0; // yellow
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j); // e.dest
+                        if (color[e.dest] == -1) {
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
+                        } else if (color[e.dest] == color[curr]) {
+                            return false; // NOT bipartite
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int verts = 5;
         ArrayList<Edge>[] graph = new ArrayList[verts];
         createGraph(graph);
+        System.out.println(isBipartite(graph));
     }
 }
