e76dce992902bfcb25b152aae21fcdcb05e596cf
Mayank <mayankchaurasiya66@gmail.com>
2025-07-11 14:03:22 +0530

    Added Bipartite Graph file

---
+++ 29. Graphs/BipartiteGraph.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BipartiteGraph {
+    static class Edge {
+        int src, dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+}
