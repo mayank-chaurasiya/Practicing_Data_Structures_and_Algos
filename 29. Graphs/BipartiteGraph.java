37275b16b852f1b6101ef3ad2793bc27cbf00f58
Mayank <mayankchaurasiya66@gmail.com>
2025-07-11 14:08:09 +0530

    Added create graph function in the BipartiteGraph file

--- 29. Graphs/BipartiteGraph.java
+++ 29. Graphs/BipartiteGraph.java
@@ -9,4 +9,31 @@ public class BipartiteGraph {
             this.dest = dest;
         }
     }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    public static void main(String[] args) {
+        int verts = 5;
+        ArrayList<Edge>[] graph = new ArrayList[verts];
+        createGraph(graph);
+    }
 }
