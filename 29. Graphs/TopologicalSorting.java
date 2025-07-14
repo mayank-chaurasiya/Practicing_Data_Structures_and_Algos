29705be07238a2c84aebbcdbaccd19f953562753
Mayank <mayankchaurasiya66@gmail.com>
2025-07-14 13:28:08 +0530

    added new file topological sorting

--- 29. Graphs/TopologicalSorting.java
+++ 29. Graphs/TopologicalSorting.java
@@ -1,3 +1,35 @@
+import java.util.*;
+
 public class TopologicalSorting {
-    
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length(); i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
+    public static void main(String[] args) {
+        int verts = 6;
+        ArrayList<Edge> graph[] = new ArrayList[verts];
+        createGraph(graph); 
+    }
 }
