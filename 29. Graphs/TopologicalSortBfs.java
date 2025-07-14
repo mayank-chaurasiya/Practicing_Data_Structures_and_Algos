c8c9f4344dff7d6965f3556960b7e9a3649c8aa5
Mayank <mayankchaurasiya66@gmail.com>
2025-07-14 19:16:06 +0530

    added a file named topological sort bfs

--- 29. Graphs/TopologicalSortBfs.java
+++ 29. Graphs/TopologicalSortBfs.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class TopologicalSortBfs {
     static class Edge {
@@ -25,6 +26,42 @@ public class TopologicalSortBfs {
 
         graph[5].add(new Edge(5, 0));
         graph[5].add(new Edge(5, 2));
+    }
+
+    public static void calcIndeg(ArrayList<Edge> graph[], int indeg[]) {
+        for (int i = 0; i < graph.length; i++) {
+            int v = i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort(ArrayList<Edge> graph[]) {
+        int indeg[] = new int[graph.length];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        // bfs
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
