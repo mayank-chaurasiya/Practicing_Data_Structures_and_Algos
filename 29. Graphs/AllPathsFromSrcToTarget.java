9b27eee62188d165b947b9a4e1d00222b3c4d4b7
Mayank <mayankchaurasiya66@gmail.com>
2025-07-15 15:21:38 +0530

    Added All Paths From Source to Target

--- 29. Graphs/AllPathsFromSrcToTarget.java
+++ 29. Graphs/AllPathsFromSrcToTarget.java
@@ -16,6 +16,8 @@ public class AllPathsFromSrcToTarget {
             graph[i] = new ArrayList<>();
         }
 
+        graph[0].add(new Edge(0, 3));
+
         graph[2].add(new Edge(2, 3));
 
         graph[3].add(new Edge(3, 1));
@@ -27,9 +29,23 @@ public class AllPathsFromSrcToTarget {
         graph[5].add(new Edge(5, 2));
     }
 
+    public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path) {
+        if (src == dest) {
+            System.out.println(path + dest);
+            return;
+        }
+
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path + src);
+        }
+    }
+
     public static void main(String[] args) {
-        int V = 6;
+        int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
+        int src = 5, dest = 1;
+        printAllPath(graph, src, dest, "");
     }
 }
