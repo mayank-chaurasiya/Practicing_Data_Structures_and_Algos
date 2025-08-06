78c616179f12b924a24a00a7c73eaef63cb04da6
Mayank <mayankchaurasiya66@gmail.com>
2025-08-06 20:43:35 +0530

    Added function Creategraph for BellmanFord Algorithm

--- 29. Graphs/BellmanFordAlgorithm.java
+++ 29. Graphs/BellmanFordAlgorithm.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class BellmanFordAlgorithm {
-    
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+    }
 }
